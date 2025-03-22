3060515a6ea611beb651888561209fe78c3995cd
JamieTattersall <627719@eastridingcollege.ac.uk>
2025-03-22 20:42:53 +0000

    Notes of what a binary search is

--- binary_search.cpp
+++ binary_search.cpp
@@ -2,6 +2,20 @@
 #include <vector>
 // Needs to be sorted
 // Halfs till target is found
+
+// Check the middle value for the value if not check if its less or more,
+// If less delete every other value above it 
+// Then repeat get middle --> greate that or less than --> delete 
+// Everytime i say delete its just disregard
+
+//          When would this be effiecnt:
+// - Only on large data sets small is just not needed
+//    - This is because you disregard HALF the elements in one its alot and can be very fast
+
+// O (log n), (time, data)
+
+
+
 int main()
 {
 
